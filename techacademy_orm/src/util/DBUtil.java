eae1a7fb37295966f86f7f1cec3d213621cbeec3
dir-FY24kidayuki <yuki.kida.l774@dirbato.co.jp>
2024-04-12 11:02:25 +0900

    Lesson11のコミット

---
+++ techacademy_orm/src/util/DBUtil.java
@@ -0,0 +1,29 @@
+package util;
+
+
+import java.io.ObjectInputStream.GetField;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class DBUtil {
+    private static final String PERSISTENCE_UNIT_NAME = "techacademy_orm";
+    private static EntityManagerFactory emf;
+    
+    
+    public static EntityManager createEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+    
+    
+    private static EntityManagerFactory getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        
+        return emf;
+    }
+
+}
